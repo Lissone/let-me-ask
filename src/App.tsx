51f6dd8b813525203e00675456c038b45b503abe
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-06-22 13:43:27 -0300

    styled new room page and navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import { Home } from './pages/Home'
+import { NewRoom } from './pages/NewRoom'
 
 import './styles/global.scss'
 
 export default function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Route path='/' exact component={Home} />
+      <Route path='/rooms/new' component={NewRoom} />
+    </BrowserRouter>
   )
 }
