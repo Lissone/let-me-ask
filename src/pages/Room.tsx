316c674ebee1de894f897ee9dda01d69de2a61be
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-06-24 11:32:07 -0300

    useRoom hook OK

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,9 +1,10 @@
-import { useState, FormEvent, useEffect } from 'react'
+import { useState, FormEvent } from 'react'
 import { useParams } from 'react-router-dom'
 
 import logoImg from '../assets/images/logo.svg'
 
 import { useAuth } from '../hooks/useAuth'
+import { useRoom } from '../hooks/useRoom'
 import { database } from '../services/firebase'
 
 import { RoomCode } from '../components/RoomCode'
@@ -11,27 +12,6 @@ import { Button } from '../components/Button'
 import { Question } from '../components/Question'
 
 import '../styles/room.scss'
-
-type FirebaseQuestions = Record<string, {
-  author: {
-    name: string
-    avatar: string
-  }
-  content: string
-  isHighlighted: boolean
-  isAnswered: boolean
-}>
-
-type QuestionType = {
-  id: string
-  author: {
-    name: string
-    avatar: string
-  }
-  content: string
-  isHighlighted: boolean
-  isAnswered: boolean
-}
 
 type RoomParams = {
   id: string
@@ -41,33 +21,11 @@ export function Room() {
   const { user } = useAuth()
   const params = useParams<RoomParams>()
 
-  const [title, setTitle] = useState('')
   const [newQuestion, setNewQuestion] = useState('')
-  const [questions, setQuestions] = useState<QuestionType[]>([])
 
   const roomId = params.id
 
-  useEffect(() => {
-    const roomRef = database.ref(`rooms/${roomId}`)
-
-    roomRef.on('value', room => { // função 'on' do firebase faz escutar quando o valor é modificado
-      const databaseRoom = room.val()
-      const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {}
-
-      const parsedQuestions = Object.entries(firebaseQuestions).map(([key, value]) => {
-        return {
-          id: key,
-          content: value.content,
-          author: value.author,
-          isHighlighted: value.isHighlighted,
-          isAnswered: value.isAnswered
-        }
-      })
-
-      setTitle(databaseRoom.title)
-      setQuestions(parsedQuestions)
-    })
-  }, [roomId])
+  const { title, questions } = useRoom(roomId)
 
   async function handleSendQuestion(event: FormEvent) {
     event.preventDefault()
